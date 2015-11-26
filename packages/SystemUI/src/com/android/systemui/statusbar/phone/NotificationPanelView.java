03eb551f1eddbeb1bd404847385ddfac5a3d96f2
David <davidlglozo@gmail.com>
2015-11-26 09:33:00 +0100

    Add custom batery icon [1/2]

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -1874,7 +1874,6 @@ public class NotificationPanelView extends PanelView implements
 
     private void setListening(boolean listening) {
         mHeader.setListening(listening);
-        mKeyguardStatusBar.setListening(listening);
         mQsPanel.setListening(listening);
     }
 
